0a6fb3710aaa8cb90e293924526383f51131b454
Gambit90s <44451389+Gambit90s@users.noreply.github.com>
2019-01-25 12:35:22 -0700

    update files 2x

--- app.js
+++ app.js
@@ -1,17 +1,62 @@
-let myFriends = ["Ronnie", "Bobbie", "Ricki", "Mike", "Brosif"]; // An array of 'myFriends'.
 
-for(let i = 0; i < myFriends.length; i++) 
-{
-let myFriend = myFriends[i];
-    console.log(myFriend);
 
-    for(let k = 99; k > 0; k--) {
-        if(k > 2) { // 99 to 2 ..."lines of"... 
-            console.log(k + " lines of code in the file, " + k + " lines of code; " + myFriend + " strikes one out, clears it all out, " + (k - 1) + " lines of code in the file.");
-        } else if(k == 2) { // 2 == plural. 
-            console.log(k + " lines of code in the file, " + k + " lines of code; " + myFriend + " strikes one out, clears it all out, " + (k - 1 ) + " line of code in the file.");
-        } else if (k == 1) { // 1 == singular.
-            console.log(k + " line of code in the file, " + k + " line of code; " + myFriend + " strikes one out, clears it all out, no more lines of code in the file!");
+document.addEventListener("DOMContentLoaded", button);
+function button() {
+    
+    let button = document.createElement("button");
+    let btnText = document.createTextNode("Sing");
+
+    button.appendChild(btnText);
+    document.body.appendChild(button);
+};
+
+
+
+document.addEventListener("click", click);
+
+function click() {
+    for (let i = 0; i < myFriends.length; i++) {
+        let div = document.createElement("div");
+        div.className = "myFriends";
+
+        let h3 = document.createElement("h3");
+        let h3Text = document.createTextNode(myFriends[i]);
+        h3.appendChild(h3Text);
+        div.appendChild(h3);
+
+        for (let j = 99; j > 0; j--) {
+            let p = document.createElement("p");
+            let pText;
+
+            if (j > 2) {
+                pText = document.createTextNode(j + "lines of code in the file," + j + "lines of code;" + myFriends[i] + "strike one outerHeight, clears it all out," + (j - 1) + "lines of code in the file");
+            } else if (j === 2) {
+                pText = document.createTextNode(j + "lines of code in the file," + j + "lines of code;" + myFriends[i] + "strike one outerHeight, clears it all out," + (j - 1) + "lines of code in the file");
+            } else {
+                pText = document.createTextNode(j + "lines of code in the file," + j + "lines of code;" + myFirends[i] + "strike one outerHeight, clears it all out, no more lines of code in the file");
+
+                p.appendChild(pText);
+                div.appendChild(p);
+            }
+            div.body.appendChild(div);
+        }
+    }
+
+
+
+    let myFriends = ["Ronnie", "Bobbie", "Ricki", "Mike", "Brosif"]; // An array of 'myFriends'.
+    function Sing() {
+        for (let i = 0; i < myFriends.length; i++) {
+
+            for (let j = 99; j > 0; j--) {
+                if (j > 2) { // 99 to 2 ..."lines of"... 
+                    console.log(j + " lines of code in the file, " + j + " lines of code; " + myFriends[i] + " strikes one out, clears it all out, " + (j - 1) + " lines of code in the file.");
+                } else if (j == 2) { // 2 == plural. 
+                    console.log(j + " lines of code in the file, " + j + " lines of code; " + myFriends[i] + " strikes one out, clears it all out, " + (j - 1) + " line of code in the file.");
+                } else  { // 1 == singular.
+                    console.log(j + " line of code in the file, " + j + " line of code; " + myFriends[i] + " strikes one out, clears it all out, no more lines of code in the file!");
+                };
+            };
         };
     };
-};  
+};
