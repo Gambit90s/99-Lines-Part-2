0815e8fe4d60eff6f829ad462198ef27ffb984fc
Gambit90s <44451389+Gambit90s@users.noreply.github.com>
2019-01-29 08:56:59 -0700

    final update

--- app.js
+++ app.js
@@ -2,7 +2,7 @@
 
 document.addEventListener("DOMContentLoaded", button);
 function button() {
-    
+
     let button = document.createElement("button");
     let btnText = document.createTextNode("Sing");
 
@@ -15,6 +15,9 @@ function button() {
 document.addEventListener("click", click);
 
 function click() {
+
+    let myFriends = ["Ronnie", "Bobbie", "Ricki", "Mike", "Brosif"]; // An array of 'myFriends'.
+
     for (let i = 0; i < myFriends.length; i++) {
         let div = document.createElement("div");
         div.className = "myFriends";
@@ -33,18 +36,21 @@ function click() {
             } else if (j === 2) {
                 pText = document.createTextNode(j + "lines of code in the file," + j + "lines of code;" + myFriends[i] + "strike one outerHeight, clears it all out," + (j - 1) + "lines of code in the file");
             } else {
-                pText = document.createTextNode(j + "lines of code in the file," + j + "lines of code;" + myFirends[i] + "strike one outerHeight, clears it all out, no more lines of code in the file");
+                pText = document.createTextNode(j + "lines of code in the file," + j + "lines of code;" + myFriends[i] + "strike one outerHeight, clears it all out, no more lines of code in the file");
 
-                p.appendChild(pText);
-                div.appendChild(p);
+
             }
-            div.body.appendChild(div);
+
+            p.appendChild(pText);
+            div.appendChild(p);
+
+            document.body.appendChild(div);
         }
     }
 
 
 
-    let myFriends = ["Ronnie", "Bobbie", "Ricki", "Mike", "Brosif"]; // An array of 'myFriends'.
+
     function Sing() {
         for (let i = 0; i < myFriends.length; i++) {
 
@@ -53,7 +59,7 @@ function click() {
                     console.log(j + " lines of code in the file, " + j + " lines of code; " + myFriends[i] + " strikes one out, clears it all out, " + (j - 1) + " lines of code in the file.");
                 } else if (j == 2) { // 2 == plural. 
                     console.log(j + " lines of code in the file, " + j + " lines of code; " + myFriends[i] + " strikes one out, clears it all out, " + (j - 1) + " line of code in the file.");
-                } else  { // 1 == singular.
+                } else { // 1 == singular.
                     console.log(j + " line of code in the file, " + j + " line of code; " + myFriends[i] + " strikes one out, clears it all out, no more lines of code in the file!");
                 };
             };
